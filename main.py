26e67329dd3f3198bdaeb82963a25fd53ed8f5e0
ilyaCoolDev <ilya@d3en.ru>
2023-02-08 11:23:24 +0300

    ТТеперь решает кваеперь реÐшает квардратные уравнения

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
-def geron(s):
-    # Формула Герона
-    a, b, c = s[0], s[1], s[2]
-    p = (a + b + c) / 2
-    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+a, b, c = map(int, input().split())
+d = b ** 2 - 4 * a * c
 
-
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
-n = len(sides)
-max_sides = 0, 0, 0
-max_s = 0
-# Перебор всех сторон
-for i in range(n):
-    for j in range(n):
-        if i != j:
-            for k in range(n):
-                if i != k and j != k:
-                    ch_sides = list(sorted([sides[i], sides[j], sides[k]]))
-                    if ch_sides[0] + ch_sides[1] > ch_sides[2]:
-                        r = geron(ch_sides)
-                        if max_s < r:
-                            max_s = r
-                            max_sides = ch_sides
-for i in max_sides:
-    print(i, end=" ")
+if d > 0:
+    x1 = (-b + d ** 0.5) / 2 * a
+    x2 = (-b - d ** 0.5) / 2 * a
+    print(x1, x2)
+elif d == 0:
+    x = -b / 2 * a
+    print(x)
+else:
+    print('Корней нет')
